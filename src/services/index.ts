7441b6855719e0e4761809ea0b122e0d2c645158
孙宇鹏 <i@cctv3.net>
2025-12-31 08:53:12 +0800

    refactor: RESTful ...

--- src/services/index.ts
+++ src/services/index.ts
@@ -13,54 +13,51 @@ export default class Services extends BaseService {
   }
 
   async login(params: {mobile: string; password: string}) {
-    let result = await this.instance.get('/public/login.do', {
+    let result = await this.instance.get('/public/login', {
       params,
     });
     return result.data;
   }
 
   async selectUser() {
-    let result = await this.instance.get('/user/selectUser.do', {});
+    let result = await this.instance.get('/users', {});
     return result.data;
   }
 
   async deleteWallet(params: {id: string}) {
-    let result = await this.instance.get('/wallet/deleteWallet.do', {params});
+    let result = await this.instance.delete(`/wallets/${params.id}`);
     return result.data;
   }
 
   async selectWallet(params: {id: string}) {
-    let result = await this.instance.get('/wallet/selectWallet.do', {params});
+    let result = await this.instance.get(`/wallets/${params.id}`);
     return result.data;
   }
 
   async selectFirstAndLastWallet() {
-    let result = await this.instance.get(
-      '/wallet/selectFirstAndLastWallet.do',
-      {},
-    );
+    let result = await this.instance.get('/wallets/first-and-last', {});
     return result.data;
   }
 
   async selectWallets(params: PaginationProps) {
-    let result = await this.instance.get('/wallet/selectWallets.do', {params});
+    let result = await this.instance.get('/wallets', {params});
     return result.data;
   }
 
   async mergeWallet(params: TWallet) {
-    let result = await this.instance.post('/wallet/mergeWallet.do', params);
+    let result = await this.instance.post('/wallets', params);
     return result.data;
   }
 
   async selectCategories(params: PaginationProps) {
-    let result = await this.instance.get('/category/selectCategories.do', {
+    let result = await this.instance.get('/categories', {
       params,
     });
     return result.data;
   }
 
   async selectPosts(params: PaginationProps, categoryId: string) {
-    let result = await this.instance.get('/post/selectPosts.do', {
+    let result = await this.instance.get('/posts', {
       params: {
         ...params,
         categoryId,
@@ -70,56 +67,42 @@ export default class Services extends BaseService {
   }
 
   async mergeCategory(c: Category) {
-    let result = await this.instance.post(`/category/mergeCategory.do`, c);
+    let result = await this.instance.post(`/categories`, c);
     return result.data;
   }
 
   async deleteCategory(id: String) {
-    let result = await this.instance.get(`/category/deleteCategory.do`, {
-      params: {id},
-    });
+    let result = await this.instance.delete(`/categories/${id}`);
     return result.data;
   }
 
   async deleteProperty(id: String) {
-    let result = await this.instance.get(`/category/deleteProperty.do`, {
-      params: {id},
-    });
-    return result.data;
-  }
-
-  async deletePassword(id: String) {
-    let result = await this.instance.get(`/deletePassword.do`, {params: {id}});
+    let result = await this.instance.delete(`/categories/${id}`);
     return result.data;
   }
 
   async mergeProperty(property: Property) {
-    let result = await this.instance.post(
-      `/category/mergeProperty.do`,
-      property,
-    );
+    let result = await this.instance.post(`/categories`, property);
     return result.data;
   }
 
   async deletePost(id: String) {
-    let result = await this.instance.get(`/post/deletePost.do`, {params: {id}});
+    let result = await this.instance.delete(`/posts/${id}`);
     return result.data;
   }
 
   async mergePost(post: Post) {
-    let result = await this.instance.post(`/post/mergePost.do`, post);
+    let result = await this.instance.post(`/posts`, post);
     return result.data;
   }
 
   async selectPost(id: String) {
-    let result = await this.instance.get(`/post/selectPost.do`, {params: {id}});
+    let result = await this.instance.get(`/posts/${id}`);
     return result.data;
   }
 
   async selectCategory(id: String) {
-    let result = await this.instance.get(`/category/selectCategory.do`, {
-      params: {id},
-    });
+    let result = await this.instance.get(`/categories/${id}`);
     return result.data;
   }
 }
